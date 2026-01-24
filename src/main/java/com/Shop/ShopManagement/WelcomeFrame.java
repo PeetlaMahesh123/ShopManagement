a44dadefe40c69051b83cd0fdc92cbf9a523e9a7
peetlamahesh <peetlamahesh81@gmail.com>
2026-01-24 14:40:01 +0530

    Updates dome to showcase output images

--- src/main/java/com/Shop/ShopManagement/WelcomeFrame.java
+++ src/main/java/com/Shop/ShopManagement/WelcomeFrame.java
@@ -30,7 +30,7 @@ public class WelcomeFrame extends JFrame {
         setBounds(100, 100, 450, 300);
 
         contentPane = new JPanel();
-        contentPane.setBackground(new Color(192, 192, 192));
+        contentPane.setBackground(new Color(128, 255, 255));
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         contentPane.setLayout(null);
         setContentPane(contentPane);
@@ -41,12 +41,12 @@ public class WelcomeFrame extends JFrame {
         contentPane.add(lbl);
 
         JButton btnUser = new JButton("User Management");
-        btnUser.setBackground(new Color(128, 128, 255));
+        btnUser.setBackground(new Color(255, 128, 128));
         btnUser.setBounds(50, 120, 150, 25);
         contentPane.add(btnUser);
 
         JButton btnProduct = new JButton("Product Management");
-        btnProduct.setBackground(new Color(128, 128, 255));
+        btnProduct.setBackground(new Color(255, 128, 128));
         btnProduct.setBounds(230, 120, 170, 25);
         contentPane.add(btnProduct);
 
